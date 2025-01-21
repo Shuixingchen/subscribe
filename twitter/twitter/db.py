80821e1bf35232d52e79b73c81cf78c00850956e
admin <admin@btcmdeMacBook-Pro.local>
2025-01-21 16:21:20 +0800

    add getpost

--- twitter/twitter/db.py
+++ twitter/twitter/db.py
@@ -17,7 +17,8 @@ class Db:
             password=password,
             database=database,
             port=int(port),
-            cursorclass=pymysql.cursors.DictCursor
+            cursorclass=pymysql.cursors.DictCursor,
+            autocommit=True  # 启用自动提交
         )
         self.cursor = self.conn.cursor()
     def get_user_id(self):
@@ -31,3 +32,23 @@ class Db:
             return 4
         except Exception as e:
             print("Error: ", e)
+    def get_big_user(self):
+        try:
+            query_big_post = """
+            SELECT username from t_big_users where status = 1
+            """
+            self.cursor.execute(query_big_post,())
+            big_users = self.cursor.fetchall()
+            return big_users
+        except Exception as e:
+            print("Error: ", e)
+    def save_big_user_post(self,data):
+        try:
+            insert_log_sql = """
+            INSERT INTO t_big_user_post (username, social,post_id,post_time) VALUES (%s,%s,%s,%s)
+            """
+            self.cursor.execute(insert_log_sql,(data['username'],data['social'],data['post_id'],data['post_time']))
+            return True
+        except Exception as e:
+            print("Error: ", e)
+            return False
--- twitter/twitter/spiders/getpost.py
+++ twitter/twitter/spiders/getpost.py
@@ -7,11 +7,12 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import pymysql
+import twitter.db as db
 import json
 import os
 from twitter.funcs import get_cookies_file
 from twitter.gptapi import GPTAPI
+from twitter.funcs import send_notice
 
 
 class GetpostSpider(scrapy.Spider):
@@ -20,52 +21,94 @@ class GetpostSpider(scrapy.Spider):
     start_urls = ["https://x.com"]
 
     def start_requests(self):
-        self.mysql_init()
-        uid = self.get_user_id()
-        print("do post uid: ", uid)
-    
-        url = "https://x.com/home"
-        file_path = get_cookies_file(uid)
-        if os.path.exists(file_path):
-            with open(file_path, 'r') as file:
-                cookies = json.load(file)
-            self.cookies = [{'key':cookie['name'], 'value':cookie['value']} for cookie in cookies]
-        else:
-            logging.info("cookies not found")
-            return
+        try:
+            self.db = db.Db()
+            uid = self.db.get_user_id()
+            print("do post uid: ", uid)
         
-        yield SeleniumRequest(url=url, callback=self.parse)
-    def parse(self, response,reply):
+            url = "https://x.com/home"
+            file_path = get_cookies_file(uid)
+            if os.path.exists(file_path):
+                with open(file_path, 'r') as file:
+                    cookies = json.load(file)
+                self.cookies = [{'key':cookie['name'], 'value':cookie['value']} for cookie in cookies]
+            else:
+                logging.info("cookies not found")
+                return
+        except:
+            logging.error("Error: start_requests")
+            logging.error(traceback.format_exc())
+            return
+        yield SeleniumRequest(url=url, callback=self.get_post)
+    def get_post(self,response):
+        try:
+            users = self.db.get_big_user()
+            for user in users:
+                if user['username'] == "":
+                    continue
+                self.parse(response,user)
+        except:
+            logging.error(traceback.format_exc())
+            return ""
+    def parse(self, response,user):
         try:
             driver = response.request.meta["driver"]
-            # 给浏览器添加Cookie
-            if hasattr(self, 'cookies'):
-                for item in self.cookies:
-                    driver.add_cookie({'name': item['key'], 'value': item['value']})
-                time.sleep(5)
-            
             # 跳到大v主页
-            driver.get("https://x.com/"+reply['user_name'])
+            driver.get("https://x.com/"+user['username'])
             wait = WebDriverWait(driver, 60)  # 设置最大等待时间为10秒
             actions = ActionChains(driver)
+            # 滚动页面
+            ActionChains(driver) \
+            .scroll_by_amount(0, 200) \
+            .perform()
             articles = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div[data-testid="tweetText"]')))
             if len(articles) == 0:
                 print("Error: ", "No articles found")
                 return ""
-            article = articles[0]
-            article_content = article.text
-            print("article: ", article_content)
-            if len(article_content) < 10:
-                print("Error: ", "No article content found")
-                return ""
-            # 鼠标移动到文章上面
-            actions.move_to_element(article).perform()
-            time.sleep(1)
-            article.click()
-            time.sleep(3)
-            last_url = driver.current_url
-            print("Current URL after click: ", last_url)
-            return last_url
+            social_pinneds = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div[data-testid="socialContext"]')))
+            social_reposted = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'span[data-testid="socialContext"]')))
+            socials = social_pinneds + social_reposted
+            headers = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div[data-testid="User-Name"]')))
+            post_headers = []
+            for i,header in enumerate(headers):
+                try:
+                    a_tag = header.find_element(By.CSS_SELECTOR, 'a[dir="ltr"][role="link"]')
+                    post_headers.append({
+                        "href": a_tag.get_attribute('href'),
+                        "time": a_tag.get_attribute('aria-label')
+                    })
+                except Exception as e:
+                    continue
+            for index, article in enumerate(articles):
+                if index < len(socials):
+                    social = socials[index]
+                    # print("index:",index,"social: ", social.text)
+                if index < len(post_headers):
+                    post_header = post_headers[index]
+                    # print("index:",index,"post_header: ", post_header)
+                # print("index:",index,"articles: ", article.text)
+                data = {
+                    "username": user['username'],
+                    "article": article.text,
+                    "post_id": post_header['href'] if index < len(post_headers) else "",
+                    "social": social.text if index < len(socials) else "",
+                    "post_time": post_header["time"] if index < len(post_headers) else "",
+                }
+                if data["post_id"] == "":
+                    continue
+                print("data: ", data)
+                res = self.db.save_big_user_post(data)
+                if res:
+                    self.send_notice("twitter", data)
         except:
             logging.error(traceback.format_exc())
             return ""
+    def send_notice(self,title,data):
+         # 构造消息内容
+        msg_lines = [title]
+        for key, value in data.items():
+            msg_lines.append(f"{key} = {value}")
+        
+        # 将所有行连接成一个字符串，并确保最后没有多余的换行符
+        msg = '\n'.join(msg_lines)
+        send_notice("https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=564f2499-fee1-42a0-8efb-7d9b65d33570", msg)
